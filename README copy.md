db333dd47bcbd636147d261a85c9ddd1d8fe0ba7
hezedu <hezedu@hotmail.com>
2022-03-07 22:33:13 +0800

    up

--- README copy.md
+++
@@ -1,8 +0,0 @@
-
-### RouteConfig.scrollBehavior
-Added a fourth parameter: `isPop` ___boolean___ to distinguish some behaviors of browsers:  
-  - If isPop is `true` it is triggered by back or forward, 
-  - else it is triggered by replace or push.
-  
-I think it's useful.
-You can use it like this: If isPop, return `savedPosition`, else scroll to top.
---
+++ components/vue-echart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="width: 100%; height: 100%" />
+</template>
+<script>
+export default {
+  methods: {
+    setOption(option){
+      this.removeChart();
+      const myChart = this.$options._cache_echart = window.echarts.init(this.$el);
+      myChart.setOption(option);
+    },
+    removeChart(){
+      if(this.$options._cache_echart){
+        this.$options._cache_echart.dispose();
+        this.$options._cache_echart = null;
+      }
+    }
+  },
+  destroyed(){
+    this.removeChart();
+  }
+}
+</script>
