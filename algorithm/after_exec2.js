d318dafb8790cb28238228fe57a79d6691038792
hezedu <hezedu@hotmail.com>
2025-03-03 17:03:12 +0800

    up

--- algorithm/after_exec2.js
+++ algorithm/after_exec2.js
@@ -4,12 +4,17 @@ function AfterExec(interval, callback){
   this.startTime = this.lastActiveTime;
   this.task = callback;
   this._timer = null;
-  this.process();
 }
+
 
 
 AfterExec.prototype.trigger = function(timeStamp){
   this.lastActiveTime = timeStamp;
+  if(this._timer){
+    return;
+  }
+  this.startTime = this.lastActiveTime;
+  this.process();
 }
 
 AfterExec.prototype.process = function(){
@@ -22,20 +27,25 @@ AfterExec.prototype.process = function(){
     interval = this.interval;
   }
   if(interval <= 0){
-    console.log('interval <= 0', interval);
+    // console.log('interval <= 0', interval);
     interval = 0;
   }
   this._timer = setTimeout(() => {
     if(this.startTime < this.lastActiveTime){
       this.process();
     } else {
+      this._timer = null;
       this.task();
     }
   }, interval);
 
 }
+
 AfterExec.prototype.destroy = function(){
   if(this._timer){
     clearTimeout(this._timer);
+    this._timer = null;
   }
 }
+
+export default AfterExec;
